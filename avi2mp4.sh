8531283a62a7c05ca967aadac8f51485def4d993
George <george@langdin.com>
2013-10-12 21:36:32 -0500

    First working version
    It works! Still much to be done...

--- avi2mp4.sh
+++ avi2mp4.sh
@@ -3,17 +3,18 @@
 DEBUG=0
 YEAR="`date +%Y`"
 VERSION=1
-CONFIG=~/stream2podcast.conf
+CONFIG=~/conf/avi2mp4.conf
+LOG=~/log/avi2mp4.`date +%F`.log
 OVERWRITE=1
 FILE_LIST=''
-SOURCE_DIR=''
-DEST_DIR=''
+SOURCE_DIR='/tmp/mst3k/avi'
+DEST_DIR='/tmp/mst3k/'
+IFS_BAK="$IFS"
 
 # Encode video as h.264 which seems to be more efficient than mpeg4
 # Set vbr quality to 20 (whatever that means) and enforce 30 fps
-# Encode audio as mp3. I don't recall why I don't use AAC. The WD box probably
-# didn't like it
-FFMPEG_OPTS='-c:v libx264 -q:v 20 -r 30 -c:a mp3'
+# Encode audio as aac. It is more efficient than mp3.
+FFMPEG_OPTS='-c:v libx264 -q:v 20 -r 30 -c:a aac -strict -2'
 
 ################################################################################
 
@@ -33,11 +34,11 @@ dep_check () {
 	done
 
 	# Check for ffmpeg codecs
-	for codec in 'V* libx264 ' 'A* mp3 '; do
-		ffmpeg -codecs 2>&1 | grep "$codec" >/dev/null 2>&1
+	for codec in 'V* libx264 ' 'A* aac '; do
+		ffmpeg -encoders 2>&1 | grep "$codec" >/dev/null 2>&1
 		if [ $? -ne 0 ]; then
 			log "ffmpeg does not have a required codec: $codec. Please install it."
-			exit 1
+#			exit 1
 		fi
 	done
 }
@@ -74,7 +75,7 @@ while getopts 'c:DrV' o; do
 	esac
 done
 
-FILE_LIST="$@"
+#FILE_LIST="$@"
 
 if [ ! -e $CONFIG ]; then
 	log "Config file ($CONFIG) does not exist. I quit."
@@ -87,7 +88,7 @@ if [ $DEBUG -eq 0 ]; then
 	exec >> $LOG 2>&1
 fi
 
-log 'stream2podcast started'
+log 'avi2mp4 started'
 
 if [ ! -d $SOURCE_DIR ]; then
 	log "SOURCE_DIR ($SOURCE_DIR) does not exist. I quit."
@@ -95,26 +96,34 @@ if [ ! -d $SOURCE_DIR ]; then
 fi
 
 if [ ! -d $DEST_DIR ]; then
-	log "DEST_DIR ($DEST_DIR) does not exist. I will create it."
-	mkdir -p $DEST_DIR
+	log "DEST_DIR ($DEST_DIR) does not exist. I quit."
+	exit 1
 fi
 
 # Check for dependancies
 dep_check
 
-# If we weren't given a list of files on the command line, scan the source
+# If we weren't given a list of files on the command line, scan the source dir
 if [ -z "$FILE_LIST" ]; then
 	if [ -z "$SOURCE_DIR" ]; then
-		log 'No files or
-	FILE_LIST=`find $SOURCE_DIR -type f`
+		log 'No files given on command line and no source directory set in config file.'
+		exit 1
+	fi
+	FILE_LIST="`find $SOURCE_DIR -maxdepth 1 -type f`"
 fi
 
-for file in "$FILE_LIST"; do
+# Set field separator to newline so we can easily handle spaces in file names
+IFS=$'\n'
+for file in $FILE_LIST; do
+	IFS=$IFS_BAK
+	file="`echo $file | sed -e 's/ /\\ /g'`"
 	# Replace suffix (i.e. .avi or .mpeg) with .mp4
-	new_name="`basename $file | sed 's/\.[[:alnum:]]\{3,4\}$/.mp4/'`"
+	new_name="`basename \"$file\" | sed -e 's/\.[[:alnum:]]\{3,4\}$/.mp4/'`"
 	if [ -f "$DEST_DIR/$new_name" -a $OVERWRITE -eq 0 ]; then
 		log "Destination file $DEST_DIR/$new_name already exists. Skipping it."
 		continue
 	fi
-	ffmpeg -i "$file" $FFMPEG_OPTS "$DEST_DIR/$new_name"
+	log "Starting $file to $DEST_DIR/$new_name"
+	ffmpeg -y -v quiet -i "$file" $FFMPEG_OPTS "${DEST_DIR}${new_name}"
+	log "Finished $file"
 done
