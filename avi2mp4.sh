c94dbbbe834064c2875ab7f50b1395173cc05038
George <george@langdin.com>
2015-11-02 20:27:29 -0600

    Clean up
    - Update subshell syntax
    - Quote variables
    - Remove unused variable

--- avi2mp4.sh
+++ avi2mp4.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 DEBUG=0
-YEAR="`date +%Y`"
 VERSION=1
 CONFIG=~/conf/avi2mp4.conf
-LOG=~/log/avi2mp4.`date +%F`.log
+LOG=~/log/avi2mp4.$(date +%F).log
 OVERWRITE=1
 FILE_LIST=''
 SOURCE_DIR='/storage/mst3k/avi'
@@ -19,14 +18,14 @@ FFMPEG_OPTS='-c:v libx264 -q:v 20 -r 30 -c:a aac -strict -2'
 ################################################################################
 
 log () {
-	echo `date +%T` "$@"
+	echo "$(date +%T) $@"
 }
 
 ################################################################################
 
 dep_check () {
 	for app in ffmpeg; do
-		which $app > /dev/null 2>&1
+		which "$app" > /dev/null 2>&1
 		if [ $? -ne 0 ]; then
 			log "$app could not be found. Please install it."
 			exit 1
@@ -35,7 +34,7 @@ dep_check () {
 
 	# Check for ffmpeg codecs
 	for codec in 'V* libx264 ' 'A* aac '; do
-		ffmpeg -encoders 2>&1 | grep "$codec" >/dev/null 2>&1
+		ffmpeg -encoders 2>&1 | grep "$codec" > /dev/null 2>&1
 		if [ $? -ne 0 ]; then
 			log "ffmpeg does not have a required codec: $codec. Please install it."
 #			exit 1
@@ -59,13 +58,13 @@ usage()
 while getopts 'c:DrV' o; do
 	case "$o" in
 	'c')
-		CONFIG=$OPTARG
+		CONFIG="$OPTARG"
 		;;
 	'D')
 		DEBUG=1
 		;;
 	'V')
-		echo $VERSION
+		echo "$VERSION"
 		exit 0
 		;;
 	'?')
@@ -75,27 +74,25 @@ while getopts 'c:DrV' o; do
 	esac
 done
 
-#FILE_LIST="$@"
-
-if [ ! -e $CONFIG ]; then
+if [ ! -e "$CONFIG" ]; then
 	log "Config file ($CONFIG) does not exist. I quit."
 	exit 1
 fi
 
-source $CONFIG
+source "$CONFIG"
 
 if [ $DEBUG -eq 0 ]; then
-	exec >> $LOG 2>&1
+	exec >> "$LOG" 2>&1
 fi
 
 log 'avi2mp4 started'
 
-if [ ! -d $SOURCE_DIR ]; then
+if [ ! -d "$SOURCE_DIR" ]; then
 	log "SOURCE_DIR ($SOURCE_DIR) does not exist. I quit."
 	exit 1
 fi
 
-if [ ! -d $DEST_DIR ]; then
+if [ ! -d "$DEST_DIR" ]; then
 	log "DEST_DIR ($DEST_DIR) does not exist. I quit."
 	exit 1
 fi
@@ -109,21 +106,21 @@ if [ -z "$FILE_LIST" ]; then
 		log 'No files given on command line and no source directory set in config file.'
 		exit 1
 	fi
-	FILE_LIST="`find $SOURCE_DIR -maxdepth 1 -type f | sort`"
+	FILE_LIST=$(find "$SOURCE_DIR" -maxdepth 1 -type f | sort)
 fi
 
 # Set field separator to newline so we can easily handle spaces in file names
 IFS=$'\n'
 for file in $FILE_LIST; do
-	IFS=$IFS_BAK
-	file="`echo $file | sed -e 's/ /\\ /g'`"
+	IFS="$IFS_BAK"
+	file=$(echo "$file" | sed -e 's/ /\\ /g')
 	# Replace suffix (i.e. .avi or .mpeg) with .mp4
-	new_name="`basename \"$file\" | sed -e 's/\.[[:alnum:]]\{3,4\}$/.mp4/'`"
-	if [ -f "$DEST_DIR/$new_name" -a $OVERWRITE -eq 0 ]; then
+	new_name=$(basename "$file" | sed -e 's/\.[[:alnum:]]\{3,4\}$/.mp4/')
+	if [ -f "$DEST_DIR/$new_name" ] && [ $OVERWRITE -eq 0 ]; then
 		log "Destination file $DEST_DIR/$new_name already exists. Skipping it."
 		continue
 	fi
 	log "Starting $file to $DEST_DIR/$new_name"
-	ffmpeg -y -v warning -i "$file" $FFMPEG_OPTS "${DEST_DIR}${new_name}"
+	ffmpeg -y -v warning -i "$file" $FFMPEG_OPTS "$DEST_DIR/$new_name"
 	log "Finished $file"
 done
